f9770a0f3084ecc615f736b2f21961925dd67b6c
Bradfordly <39799881+Bradfordly@users.noreply.github.com>
2025-12-03 04:16:41 +0000

    chore: gamehost scripts

---
+++ gamehost/scripts/userdata.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -xe
+
+### install dependencies ###
+depList=(
+    awscli
+    curl
+    wget
+    file
+    tar
+    bzip2
+    gzip
+    unzip
+    bsdmainutils
+    python3
+    util-linux
+    ca-certificates
+    binutils
+    bc
+    jq
+    tmux
+    netcat
+    lib32gcc-s1
+    lib32stdc++6
+    libsdl2-2.0-0:i386
+)
+dpkg --add-architecture i386
+apt update
+apt install ${depList[@]} -y
+
+### stage linuxgsm script ###
+curl -Lo linuxgsm.sh https://linuxgsm.sh
+chmod +x linuxgsm.sh
+
+### install game server files ###
+serverList=(
+    pwserver    #Palworld
+    mcserver    #Minecraft
+    vhserver    #Valheim
+)
+for server in "${!serverList[@]}"
+do
+    adduser --system $server --disabled-password
+    #password= ;)
+    #echo "$server:$password" | chpasswd
+    echo "$server  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+    chown -R "$server:$server" /home/$server
+    cp linuxgsm.sh /home/$server/linuxgsm.sh 
+    /bin/su -c "/home/$server/linuxgsm.sh $server" - $server
+    bash /home/$server/$server install 
+    /bin/su -c "/home/$server/$server auto-install" - $server
+done
+
+### dynamic dns ###
+chmod +x /opt/gamehost/scripts/ddns.sh
+bash /opt/gamehost/scripts/ddns.sh
+cp /opt/gamehost/scripts/ddns.sh /etc/init.d/ddns.sh
+
+### backup schedule ###
+cp /opt/gamehost/scripts/backup.sh /etc/cron.daily/backup.sh
